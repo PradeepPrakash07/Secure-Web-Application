e256495d2eaa490d087184ed054002a86440dd81
PradeepPrakash07 <pradeepnagalu@gmail.com>
2022-12-10 12:53:23 +0000

    Admin code

---
+++ admin/adminLogin.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $adminName = $_POST["Name"];
+    $password = $_POST["pass"];
+    require_once '../include/inc_db_handler.php';
+    require_once 'inc_functions.php';
+    if (checkEmptylogin($adminName, $password) !== false) {
+        header("location: ../index.php?error=emptyfield");
+        exit();
+    }
+    validateAdminLogin($password, $conn, $adminName);
+    session_start();
+    $_SESSION['admin'] = 'TRUE';
+    header("location: dashboard.php");
+    exit();
+}
+else {
+    header("location: index.php?error=noadmin");
+
+}
