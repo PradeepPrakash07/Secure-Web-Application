6a12f3de0de0878cba53ef3fb97ed2fe276fc4d9
PradeepPrakash07 <pradeepnagalu@gmail.com>
2022-12-15 22:44:49 +0000

    Features implementation

---
+++ include/inc_book.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (isset($_POST["submit"])) {
+    $date = $_POST["doa"];
+    // echo($date);
+    if (empty($date)) {
+        header("location: ../appointment.php?err=slctdt");
+        exit();
+    }
+    include_once("inc_db_handler.php");
+    $date = date(htmlentities($_POST["doa"]));
+    $pnmbr = $_POST["passport_number"];
+    $name = $_SESSION['name'];
+    $uid = $_SESSION['uid'];
+    $bid = substr($name,0,3).(string)rand(100000,999999);
+    $sql = "INSERT INTO bookings(userName , usrId, date, bookingId, passportNumber) VALUES ('$name', '$uid', '$date', '$bid', '$pnmbr');"; //creates booking entry in the database//
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        $_SESSION['bid'] = $bid;
+        $_SESSION['dt'] = $date;
+        header("location: ../appointment.php?msg=booked");
+        exit();
+    } else {
+        header("location: ../appointment.php?msg=errbkng");
+        exit();
+    }
+
+
+
+} else {
+    header("location: ../appointment.php?err=slctdt");
+    exit();
+}
---
+++ include/inc_db_handler.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+//for establishing connection to the database//
+
+
+
+$servername = "localhost"; //change to your server name//
+
+$dbUsername = ""; // change to your databse user name//
+
+$dbPassword = ""; //change to your database password//
+
+$dbName = "registration"; //change to your data base name//
+
+
+
+$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName); //create connection//
+
+
+
+if (!$conn) {
+
+    die("Connection failed : " . mysqli_connect_error()); //if failed to create connection then exit//
+
+}
